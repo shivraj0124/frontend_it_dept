43e8ad8d1ca25d645a09a926ee54bd2b9f84fdeb
shivraj <sk40fs@gmail.com>
2023-10-24 19:34:10 +0530

    'tt'

--- src/Components/Admin/AddTT.jsx
+++ src/Components/Admin/AddTT.jsx
@@ -27,7 +27,7 @@ function AddTT() {
         setPhoto(file);
     };
 
-    const allSem =async () => {
+    const allSem = async () => {
         try {
             const response = await axios.get(`${urlBackend}/api/v1/get-semesters`);
 
@@ -42,7 +42,7 @@ function AddTT() {
 
     };
 
-    const allShifts =async (selectedSemesterId) => {
+    const allShifts = async (selectedSemesterId) => {
         try {
             const response = await axios.get(`${urlBackend}/api/v1/get-shifts/${selectedSemesterId}`);
 
@@ -67,11 +67,7 @@ function AddTT() {
         formData.append('shift', selectedShift);
 
         try {
-            const response = await axios.post(`${urlBackend}/api/v1/addTT`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
+            const response = await axios.post(`${urlBackend}/api/v1/addTT`, formData);
 
             if (response.data.success) {
                 toast.success('Time Table Uploaded Successfully!', {
@@ -99,44 +95,45 @@ function AddTT() {
 
     return (
         <div className='h-screen overflow-y-scroll pb-10 bg-blue-50'>
-            <div className='w-100 mt-16  justify-center max-xl:px-2 xl:px-36 pb-10 max-md:mt-20'>
+            <div className='w-100 mt-2 justify-center max-xl:px-2 xl:px-36 pb-10 max-md:mt-20'>
                 <div className='w-[100%] flex md:flex-row- max-md:flex-col-reverse px-2 bg-white shadow-xl rounded-md'>
                     <div className='w-[100%] text-center  bg-white pb-10 mt-10 '>
                         <h1 className='text-center font-semibold text-2xl underline underline-offset-4'>Add Time Table</h1>
-
-                        <div className='mt-6 flex max-md:flex-col justify-between  items-center md:flex-row px-12 '>
-                            <label htmlFor='semesters' className='font-semibold text-xl'>
-                                Select Semester:
-                            </label>
-                            <Select
-                                id='semesters'
-                                className='max-md:w-[80%] md:w-[57%] font-semibold md:ml-3 focus:outline-none'
-                                placeholder='Select Semester'
-                                onChange={handleChangeSemester}
-                            >
-                                {semesterList?.map((semester) => (
-                                    <Option key={semester._id} value={semester._id}>
-                                        {semester.name}
-                                    </Option>
-                                ))}
-                            </Select>
-                        </div>
-                        <div className='mt-4 flex max-md:flex-col justify-between  items-center md:flex-row px-12 '>
-                            <label htmlFor='shifts' className='font-semibold text-xl'>
-                                Select Shift:
-                            </label>
-                            <Select
-                                id='shifts'
-                                className='max-md:w-[80%] md:w-[57%] font-semibold md:ml-3 focus:outline-none'
-                                placeholder='Select Shift'
-                                onChange={handleChangeShift}
-                            >
-                                {shiftList?.map((shift) => (
-                                    <Option key={shift._id} value={shift._id}>
-                                        {shift.name}
-                                    </Option>
-                                ))}
-                            </Select>
+                        <div className='w-[100%] flex flex-col justify-center items-center px-[10%]'>
+                            <div className='mt-6 flex max-md:flex-col justify-between  items-center md:flex-row  w-[100%]'>
+                                <label htmlFor='semesters' className='font-semibold text-xl'>
+                                    Select Semester:
+                                </label>
+                                <Select
+                                    id='semesters'
+                                    className='max-md:w-[100%] md:w-[57%] font-semibold md:ml-3 focus:outline-none'
+                                    placeholder='Select Semester'
+                                    onChange={handleChangeSemester}
+                                >
+                                    {semesterList?.map((semester) => (
+                                        <Option key={semester._id} value={semester._id}>
+                                            {semester.name}
+                                        </Option>
+                                    ))}
+                                </Select>
+                            </div>
+                            <div className='mt-4 flex max-md:flex-col justify-between  items-center md:flex-row w-[100%]'>
+                                <label htmlFor='shifts' className='font-semibold text-xl'>
+                                    Select Shift:
+                                </label>
+                                <Select
+                                    id='shifts'
+                                    className='max-md:w-[80%] md:w-[57%] font-semibold md:ml-3 focus:outline-none'
+                                    placeholder='Select Shift'
+                                    onChange={handleChangeShift}
+                                >
+                                    {shiftList?.map((shift) => (
+                                        <Option key={shift._id} value={shift._id}>
+                                            {shift.name}
+                                        </Option>
+                                    ))}
+                                </Select>
+                            </div>
                         </div>
                         <form className='mt-2 text-black' onSubmit={handleOnSubmit}>
                             <input
@@ -151,13 +148,13 @@ function AddTT() {
                                 Upload Time Table
                             </label>
                             <br />
-                            <button className='mt-16 max-md:mt-5 w-[80%] bg-blue-800 rounded-lg py-2 text-xl text-white cursor-pointer hover:bg-blue-500'>
+                            <button className='mt-8 max-md:mt-5 w-[80%] bg-blue-800 rounded-lg py-2 text-xl text-white cursor-pointer hover:bg-blue-500'>
                                 Add Time Table
                             </button>
                         </form>
                     </div>
-                    <div className='w-[100%] h-[70%] mt-16 max-md:mt-2'>
-                        <img className='w-[100%] h-[100%] max-md:h-[70%] max-md:hidden' src={img1} alt='' />
+                    <div className='w-[100%] h-[100%] max-xl:hidden max-md:mt-2'>
+                        <img className='w-[100%]  ' src={img1} alt='' />
                     </div>
                 </div>
             </div>
