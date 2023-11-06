b7b034ee12ca9479d6e6b120c80dc24c16f5844a
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-11-06 06:40:35 +0530

    Update HODDesk.jsx

--- src/Components/HomeComponents/HODDesk.jsx
+++ src/Components/HomeComponents/HODDesk.jsx
@@ -4,7 +4,7 @@ import {LazyLoadImage} from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const HODDesk = () => {
     return (
-        <div className="  w-[100%] md:px-[12%] max-mad:px-[2%]  items-center justify-center py-12 bg-white">
+        <div className="  w-[100%] md:px-[12%] max-mad:px-[4%]  items-center justify-center py-12 bg-white">
 
             <div className="flex md:flex-row flex-col w-[100%] items-center justify-center md:gap-1 md:font-serif">
 
@@ -15,7 +15,7 @@ const HODDesk = () => {
                 </div>
                 <div className="w-[100%] mt-5 px-2">
                     <p className='font-bold text-black text-2xl'>Dear<span className='text-blue-700'> Colleagues and Students,</span></p>
-                    <p className='text-md mt-5 font-sans text-gray-500'>The department aims at addressing essential challenges faced by IT Industry, society and the academia. Every IT Industry is working on upcoming technologies like Artificial Intelligence, Machine Learning, Deep Learning, Blockchain, Internet of Things, Cloud computing, etc. Being an autonomous institute, we offer Outcome based Education to students in which curriculum is designed as per the present demand of the industry. Six months inplant training is part of our curriculum wherein students are placed in industry for training.
+                    <p className='text-md mt-5 font-sans text-gray-500 flex justify-between'>The department aims at addressing essential challenges faced by IT Industry, society and the academia. Every IT Industry is working on upcoming technologies like Artificial Intelligence, Machine Learning, Deep Learning, Blockchain, Internet of Things, Cloud computing, etc. Being an autonomous institute, we offer Outcome based Education to students in which curriculum is designed as per the present demand of the industry. Six months inplant training is part of our curriculum wherein students are placed in industry for training.
 
                         We believe in upholding our unceasing commitment to our students, helping them to learn, grow, develop, and achieve their goals in their pursuit to excel in their professional career.
 
