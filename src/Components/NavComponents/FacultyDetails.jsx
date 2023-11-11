cc784c81a0c1b3aded1ec4468016ca058a27da18
shivraj <sk40fs@gmail.com>
2023-11-11 10:25:56 +0530

    'new'

--- src/Components/NavComponents/FacultyDetails.jsx
+++ src/Components/NavComponents/FacultyDetails.jsx
@@ -36,10 +36,10 @@ function FacultyDetails() {
         <div className='w-[85%] py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 gap-x-4 justify-items-center md:px-[5%] bg-gray-100  mt-2 '>
           {
             facultyList?.map((faculty, index) => (
-              <div className='bg-blue-50 flex flex-col shadow-2xl w-[90%] max-md:w-[100%] justify-between items-center rounded-xl p-3'>
-                <div className=''><img src={faculty.photo} alt="" className='w-[280px] h-[200px] rounded-3xl' /></div>
-                <div className='font-bold text-lg mt-5 w-[110%] flex flex-col px-5'>
-                  <h1 className='font-semibold ml-0 '>Name:<p className='text-gray-800'>{faculty.name}</p> </h1>
+              <div className='bg-blue-50 flex flex-col shadow-2xl md:w-[90%]  max-md:max justify-between items-center rounded-3xl p-3 pt-auto'>
+                <div className='md:pt-2'><img src={faculty.photo} alt="" className='w-[280px] h-[200px] rounded-3xl' /></div>
+                <div className='font-bold text-lg mt-5 w-[100%] flex flex-col px-5'>
+                  <h1 className='text-gray-700'>{faculty.name}</h1>
                   <p className='text-gray-700'>{faculty.post}</p>
                   <p className='text-gray-700'>{faculty.qualification}</p>
                   <h1 className='text-gray-700'>Teaching Experience: {faculty.experience}</h1>
